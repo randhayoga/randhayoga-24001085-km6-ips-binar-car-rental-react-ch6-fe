f09857ff2871611efae008cb18ca91486a10da5e
randhayoga <92174528+randhayoga@users.noreply.github.com>
2024-05-12 10:29:01 +0700

    feat: connect profile page to db

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,6 +1,19 @@
 import { Card, Image, Form, InputGroup, Button } from "react-bootstrap";
 
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getProfile } from "../../redux/actions/auth";
+
 const Profile = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const { user } = useSelector((state) => state.auth);
+    useEffect(() => {
+        dispatch(getProfile(navigate, null, "/login"));
+    }, [dispatch, navigate]);
+
     return (
         <>
             <style type="text/css">
@@ -18,44 +31,48 @@ const Profile = () => {
 
             <Card>
                 <Card.Body>
-                    <Card.Title className="text-center fs-2 mb-3">
-                        Your Profile
-                    </Card.Title>
-                    <div className="d-flex flex-column align-items-center mb-4">
-                        <Image
-                            src="images/profileTemp.jpg"
-                            className="img-profile"
-                            roundedCircle
-                        />
-                    </div>
-                    <InputGroup className="mb-3">
-                        <InputGroup.Text
-                            id="nama-addon"
-                            className="custom-label"
-                        >
-                            Nama
-                        </InputGroup.Text>
-                        <Form.Control
-                            placeholder="Linus Sebastian" // fetch from API
-                            aria-label="nama"
-                            aria-describedby="nama-addon"
-                            disabled
-                        />
-                    </InputGroup>
-                    <InputGroup className="mb-3">
-                        <InputGroup.Text
-                            id="mail-addon"
-                            className="custom-label"
-                        >
-                            Email
-                        </InputGroup.Text>
-                        <Form.Control
-                            placeholder="linussebastian@binar.com" // fetch from API
-                            aria-label="mail"
-                            aria-describedby="mail-addon"
-                            disabled
-                        />
-                    </InputGroup>
+                    <Form>
+                        <Card.Title className="text-center fs-2 mb-3">
+                            Your Profile
+                        </Card.Title>
+                        <div className="d-flex flex-column align-items-center mb-4">
+                            {user?.photo && (
+                                <Image
+                                    src="images/profileTemp.jpg"
+                                    className="img-profile"
+                                    roundedCircle
+                                />
+                            )}
+                        </div>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text
+                                id="nama-addon"
+                                className="custom-label"
+                            >
+                                Nama
+                            </InputGroup.Text>
+                            <Form.Control
+                                aria-label="nama"
+                                aria-describedby="nama-addon"
+                                value={user?.name}
+                                disabled
+                            />
+                        </InputGroup>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text
+                                id="mail-addon"
+                                className="custom-label"
+                            >
+                                Email
+                            </InputGroup.Text>
+                            <Form.Control
+                                aria-label="mail"
+                                aria-describedby="mail-addon"
+                                value={user?.email}
+                                disabled
+                            />
+                        </InputGroup>
+                    </Form>
                 </Card.Body>
             </Card>
         </>
